600aacc31f0077f6d282a49e23c19b9f0804f4f2
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-04-26 11:08:21 -0300

    Calendar business logic

--- CalendarApp/src/app/calendar/calendar.component.ts
+++ CalendarApp/src/app/calendar/calendar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CalendarService } from './calendar.service';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit{
+  currentMonth:string = 'jan';
+  days;
 
+  constructor(private route: ActivatedRoute, private service: CalendarService){}
+
+  ngOnInit(): void {
+      this.currentMonth = this.route.snapshot.params['month'];
+
+      this.days = this.service.getMonthDays(this.service.getMonthNumber(this.currentMonth));
+  }
 }
