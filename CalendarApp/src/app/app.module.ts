5fda66f863da6ecdc1bda5817a7d0065fc786bd4
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-04-26 08:52:48 -0300

    Components created

--- CalendarApp/src/app/app.module.ts
+++ CalendarApp/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CdkTableModule } from '@angular/cdk/table';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { EventComponent } from './event/event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    EventComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CdkTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
